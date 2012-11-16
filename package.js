364f01973026921d725a93f7d06bd41024db4287
Eric Robitaille <eric@vianet.ca>
2012-11-16 08:19:33 -0500

    Initial commit

---
+++ package.js
@@ -0,0 +1,29 @@
+Package.describe({
+  summary: "Bootstrap styled version of login widgets"
+});
+
+Package.on_use(function (api) {
+  api.use(['accounts-urls', 'accounts-base', 'underscore', 'templating'], 'client');
+
+  api.add_files([
+    'accounts_ui.js',
+
+    'login_buttons_images.css',
+    'login_buttons.html',
+    'login_buttons_single.html',
+    'login_buttons_dropdown.html',
+    'login_buttons_dialogs.html',
+
+    'login_buttons_session.js',
+
+    'login_buttons.js',
+    'login_buttons_single.js',
+    'login_buttons_dropdown.js',
+    'login_buttons_dialogs.js'], 'client');
+});
+
+Package.on_test(function (api) {
+  //api.use('meteor-accounts-ui-bootstrap');
+  //api.use('tinytest');
+  //api.add_files('accounts_ui_tests.js', 'client');
+});
