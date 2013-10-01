188d102f507ca713b47d8ebb506a8d3886b06c92
Aykut Yaman <yaman.ayk@gmail.com>
2013-10-01 13:10:33 +0300

    initial commit

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  summary: "Bootstrap styled version of login widgets"
+  summary: "Bootstrap styled version of login widgets for Turkish"
 });
 
 Package.on_use(function (api) {
@@ -35,7 +35,7 @@ Package.on_use(function (api) {
 });
 
 Package.on_test(function (api) {
-  //api.use('meteor-accounts-ui-bootstrap');
-  //api.use('tinytest');
-  //api.add_files('accounts_ui_tests.js', 'client');
+  api.use('meteor-accounts-ui-bootstrap');
+  api.use('tinytest');
+  api.add_files('accounts_ui_tests.js', 'client');
 });
